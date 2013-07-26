6dc71dbffd4539e4a06553669bd6d3bd28af4bd6
CoolSwat <mate.swati@gmail.com>
2013-07-26 20:38:45 +0530

    First commit

---
+++ index.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>:: Welcome to Goodwill City ::</title>
+<link href="css/style.css" rel="stylesheet" type="text/css" />
+
+<!-- Get Google CDN's jQuery and jQuery UI -->
+<script src="js/jquery.min.js"></script>
+<script src="js/jquery-ui.min.js"></script>
+<!-- slidebox styling via external css -->
+<link rel="stylesheet" href="css/jquery.mSimpleSlidebox.css">
+<!-- slidebox plugin -->
+<script src="js/jquery.mSimpleSlidebox.js"></script>
+<!-- slidebox function call -->
+<script type="text/javascript">
+$(document).ready(function(){
+	$(".slidebox").mSlidebox();
+});
+</script>
+</head>
+
+<body>
+<div class="header_section">
+<div class="logo"><a href="index.php"><img src="images/logo.png" alt="" border="0" /></a></div>
+<div class="social_icon"><div style="float:left; overflow:hidden; padding-right:10px;">Join with Us :</div><a href="#"><img src="images/twitter.png" alt="" border="0" /></a> <a href="#"><img src="images/fb.png" alt="" border="0" /></a> <a href="#"><img src="images/img_09.png" alt="" border="0" /></a></div>
+</div>
+
+<div class="banner_section">
+<div class="slidebox">
+		<ul>
+    		<!--<li>
+        		<div><img src="images/banner_01.jpg" alt="" /></div>
+        	</li>-->
+<li><div><img src="images/banner-home-1.jpg" alt=""  height="560" width="1000"/></div></li>
+        	<li>
+        		<div><img src="images/banner_02.jpg" alt="" height="560" width="1000"/></div>
+        	</li>
+            <li><div><img src="images/banner_08.jpg" alt=""  height="560" width="1000"/></div></li>
+
+<li><div><img src="images/banner-home-2.jpg" alt=""  height="560" width="1000"/></div></li>
+
+
+		</ul>
+	</div>
+    <div class="navigation_section">
+    <div class="navigation">
+    <ul><li><a href="index.php" class="current">Home</a></li>
+    <li><a href="about_us.html">About Us</a></li>
+    <li><a href="projects.php">Projects</a></li>
+    <li><a href="book_plot.php">Book a Plot</a></li>
+    <li><a href="photo_gallery.html">Photo Gallery</a></li>
+    <li><a href="send_enquiry.html">Send Enquiry</a></li>
+    <li><a href="contact_us.html">Contact</a></li></ul>
+    </div>
+    </div>
+    </div>
+    
+    <div class="main_container">
+    <div class="left_content">
+    <h1>Welcome to Goodwill City Landmark !</h1>
+    <p>Welcome to Goodwill City Landmark Developers Pvt. Ltd. we are proud to feature our landmark project “Switzer Valley” at Kasarewadi, Takawe, Kanhe Phata, Pune. An Exclusive society of Bungalow Plots. The project is spanning over nearly 16 acres and each plot is equipped with amenities.</p>
+
+<p>If you believe that you missed a number if investment opportunities in Pune, we suggest you THINK AGAIN ! “Switzer Valley” is situated near Kasarewadi, only 50 KMS from Pune and not more than 7kms from the main Pune Mumbai Highway. “Switzer Valley: produces a golden opportunity for you to invest your capital securely, in and around the industrial belt of Talegaon-Chakan MIDC area.
+
+  </p>
+<p><strong></strong><br />
+Plots here are with collector sanctioned N/A. The scenic view around this project makes one feel blessed by Nature along with being blesses with its heavenly view. This project produces you with pure running water and continues electricity, well planted tar coasted roads for internal connections and security toward your plots. This is sure to be a whole package for your sage investment and in turn gaining quick appreciation.</p>
+    <h2></h2>
+    <div class="thumb_box">
+      
+      </div>
+
+    </div>
+
+    <div class="right_form">
+    <h1>Find your Plots</h1>
+    <form method="post" action="book_plot.php">
+    Username:<br />
+    <input name="name" type="text" size="40" class="input" /><br />
+    Password:<br />
+    <input name="mobile" type="text" size="40" class="input" /><br />
+    <!--Submit Query:<br />
+    <textarea name="comments" cols="31" rows="5" class="input"></textarea><br />-->
+    <input name="submit" type="Submit" class="submit_button" />
+    </form>
+    </div>
+    </div>
+
+<!-- <div style="border:0px solid red; float:left; width:100%;padding-left:235px">
+<img src="images/banner_02.jpg" alt="" height="150" width="150"/>
+<img src="images/banner_02.jpg" alt="" height="150" width="150"/>
+<img src="images/banner_02.jpg" alt="" height="150" width="150"/>
+<img src="images/banner_02.jpg" alt="" height="150" width="150"/>
+</div> -->    
+    <div class="footer_section">~
+    <div class="footer"><ul>
+      <li><a href="index.php" class="current">Home</a></li>
+    <li><a href="about_us.html">About Us</a></li>
+    <li><a href="projects.php">Projects</a></li>
+    <li><a href="photo_gallery.html">Photo Gallery</a></li>
+    <li><a href="send_enquiry.html">Send Enquiry</a></li>
+    <li><a href="contact_us.html">Contact Us</a></li>
+    </ul></div>
+    </div>
+</body>
+</html>
